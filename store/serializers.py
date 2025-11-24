3c091edc11c22eb1ca3b23a682dd6d7cb6bb96b9
Obaro Awowede <akpobaroawowede@gmail.com>
2025-11-24 15:14:06 +0100

    feat: Created Order serializer, handling the logic of creating orders, updating stock and calculating total price

--- store/serializers.py
+++ store/serializers.py
@@ -6,7 +6,7 @@ from .models import (
 )
 from django.db.models import Avg
 
-class UserSerializers(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id','username','email', 'first_name', 'last_name')
@@ -85,7 +85,7 @@ class ProductSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('Stock can not be negative')
         return value
         
-class Address(serializers.ModelSerializer):
+class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = (
@@ -108,3 +108,90 @@ class OrderItemSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('Quantity must be more than 0')
         return value
         
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    items_data = OrderItemSerializer(many=True,write_only= True, source ='items')
+    shipping_address = AddressSerializer(read_only=True)
+    billing_address = AddressSerializer(read_only=True)
+    shipping_address_id = serializers.PrimaryKeyRelatedField(
+        queryset=Address.objects.all(),
+        source='shipping_address',
+        write_only=True
+    )
+    billing_address_id = serializers.PrimaryKeyRelatedField(
+        queryset=Address.objects.all(),
+        source='billing_address',
+        write_only=True,
+        required=False
+    )
+    user_info = UserSerializer(source='user',read_only=True)
+    item_count = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = Order
+        fields = (
+            'id', 'order_number', 'user_info', 'shipping_address', 
+            'billing_address', 'shipping_address_id', 'billing_address_id',
+            'order_status', 'payment_status', 'subtotal', 'shipping_cost', 
+            'total', 'notes', 'items', 'items_data', 'item_count',
+            'created_at', 'updated_at'
+        )
+        read_only_fields = ('id', 'order_number', 'subtotal', 'total', 'created_at', 'updated_at')
+    
+    def get_item_count(self, obj):
+        return obj.items.count()
+    
+    def validate_items_data(self, value):
+        if not value:
+            raise serializers.ValidationError('Order must include at least one item')
+        return value
+    
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        user = self.context['request'].user
+        
+        #below i'm calculating the subtotal
+        subtotal = Decimal('0.00')
+        for item in items_data:
+            product = item['product']
+            quantity = item['quantity']
+            
+            #checking if stock quantity ordered is available
+            if product.stock < quantity:
+                raise serializers.ValidationError(
+                    f"Insufficient stock for {product.name}, Available: {product.stock}"
+                )
+                
+            subtotal = subtotal + (product.price * quantity)
+        
+        #calculating total price
+        shipping_cost = validated_data.get('shipping_cost', Decimal('0.00'))
+        total = subtotal + shipping_cost
+        
+        #using shipping_address if billing_address is not given
+        if 'billing_address' not in validated_data:
+            validated_data['billing_address'] = validated_data['shipping_address']
+        
+        #creating the order
+        order = Order.objects.create(
+            user=user,
+            subtotal=subtotal,
+            total=total,
+            **validated_data
+        )
+        
+        #creating the order items and updating stock
+        for item in items_data:
+            product = items_data['product']
+            quantity = items_data['quantity']
+                        
+            OrderItem.objects.create(
+                order=order,
+                product=product,
+                quantity=quantity,
+                price=product.price
+            )
+            
+            product.stock = product.stock - quantity
+            product.save()
+        return order
