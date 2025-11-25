6ef5c35e4502b4a337d739ef3d4e49d84124b2e3
Obaro Awowede <akpobaroawowede@gmail.com>
2025-11-25 14:22:42 +0100

    feat: created user viewset and started category viewset

--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render  # type: ignore
-from rest_framework import generics, permissions, status # type: ignore
+from rest_framework import generics, permissions, status, viewsets # type: ignore
+from rest_framework.decorators import action # type: ignore
 from rest_framework.response import Response # type: ignore
 from rest_framework_simplejwt.tokens import RefreshToken # type: ignore
-from .serializers import UserRegistrationSerializer
+from .serializers import UserRegistrationSerializer, UserSerializer, CategorySerializer
+from .models import User, Category, Cart, CartItem, Product, ProductImage, Address, Order, OrderItem, Review
 
 def get_token_for_user(user):
     refresh = RefreshToken.for_user(user)
@@ -25,3 +27,18 @@ class UserRegisterView(generics.GenericAPIView):
             'access': tokens['access'],
             'refresh': tokens['refresh'],
         }, status=status.HTTP_201_CREATED)
+        
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def me(self,request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+    
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permissions = [permissions.IsAuthenticatedOrReadOnly]
